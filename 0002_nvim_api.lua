05c1275446bceb6914e7cb1b4f762587c5738e52
jimrothstein <jimrothstein@gmail.com>
2021-03-15 01:02:31 -0700

    wip

---
+++ 0002_nvim_api.lua
@@ -0,0 +1,5 @@
+--  These are vim api ,  called by lua
+
+-- Toggle 
+-- :lua vim.api.nvim_command('set number!')
+-- :lua vim.api.nvim_command('echo "Hello, Nvim!"')
---
+++ lua_file.lua
@@ -0,0 +1,8 @@
+
+
+
+-- This is a .lua file
+-- To source it from .vim:   :luafile <file>
+-- :luafile % will also work.
+x = "hello"
+print(x)

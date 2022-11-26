85dfc2ce5bf090972a1202d52b44082320278817
jimrothstein <jimrothstein@gmail.com>
2022-11-26 00:27:28 -0800

    wip

---
+++ 10_mapping_to_run_function.lua
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------
+-- PURPOSE:  Write a lua function.  Trigger that function with a mapping.
+-- USAGE:    :source % then type the shortcut.
+-- NEXT:      connect to quickfix !
+-------------------------------------------------------------------------
+local opts = { noremap=true, silent=true }
+
+local function doWork()
+      print("triggered by quickfix")
+--     vim.lsp.buf.code_action({
+--         filter = function(a) return a.isPreferred end,
+--         apply = true
+--    })
+end
+
+
+vim.keymap.set('n', '<leader>X', doWork, opts)

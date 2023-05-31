3f2f8a2dc5a56f4241c170987f712f9d13282b0b
jimrothstein <jimrothstein@gmail.com>
2023-05-30 19:31:32 -0700

    wip

--- 020_more_cheat.lua
+++ 020_more_cheat.lua
@@ -1,14 +1,25 @@
 -- file <- "020_more_cheat.lua-"
 --	PUROSE:  Random lua practice
---	TODO:		 Go through and keep useful only.
+--	TAGS:   multiline comments, expand()
 --
--- LUA
-
---  NOTES  --------------------------------------------------  
--- This is a .lua file
--- To source it from .vim:   :luafile <file>
--- :luafile % will also work.
- -------------------------------------------------- 
+-- [[
+--	TODO:
+-- 30MAY2023
+-- Go through and keep useful only.-
+--   some things do NOT work; not sure why, spell check?, :wqq:
+--
+--------------------------------------------------
+--  NOTES  --------------------------------------------------
+--  PURPOSE:
+--  USAGE:    2,3source %
+--              OR 2,3so
+--
+--  DO NOT source:  (unexpected results !)
+--      so %
+--      :luafile %
+--------------------------------------------------
+-- ]]
+--
 --
 --[[
 Multiline comment ...
@@ -17,15 +28,14 @@ In lua, nil or false evaluate to:  false
 --]]
 
 --[[
-~/code/lua_project/
-
 Lua + neovim:
-    *  code is lua.
-    *  but calls the neovim API | look careful, can see the vim
+    *   calls the neovim API | look careful, can see the vim
         *  api.nvim...command("enew") -- creates new file and edits.
         *  vim.bo[0],buftype=nofile
 --]]
 --
+--
+--[[
  -------------------------------------------------- 
 --  These are vim api ,  called by lua
  -------------------------------------------------- 
@@ -35,36 +45,36 @@ local cmd = vim.cmd
 cmd("pwd")   -- execute vim Ex: command 
 
 -- current file name:  
-:lua print(vim.fn.expand('%')  
+print(vim.fn.expand('%'))
 
 --  set vim options
 :lua vim.api.nvim_command('set nonumber')   
 :lua vim.api.nvim_command('set number!')        -- toggle
-:lua vim.api.nvim_command('echo "Hello, Nvim!"')
+vim.api.nvim_command('echo "Hello, Nvim!"')
 
 --  list buffers, vim.cmd is alias for vim.api.nvim_exec()
--- lua vim.cmd('buffers') 
+vim.cmd('buffers') 
+vim.api.nvim_exec('buffers')   -- TODO not working
 
 -- print
---lua print(_VERSION)
--- lua print("hi")
+print(_VERSION)
+print("hi")
 
  -------------------------------------------------- 
 -- print, datatypes
 -- Data types are converted correctly
+ -------------------------------------------------- 
 print(vim.api.nvim_eval('1 + 1')) -- 2
-print(vim.inspect(vim.api.nvim_eval('[1, 2, 3]'))) -- { 1, 2, 3 }
+print(vim.inspect(vim.api.nvim_eval('[1, 2, 3]'))) -- prints { 1, 2, 3 }
 print(vim.inspect(vim.api.nvim_eval('{"foo": "bar", "baz": "qux"}'))) -- { baz = "qux", foo = "bar" }
 print(vim.api.nvim_eval('v:true')) -- true
 print(vim.api.nvim_eval('v:null')) -- nil
 
-
-vim.api.nvim_command('new')
+-- vim.api.nvim_command('new')    ## opens new, unnamed buffer
 
 -------------------------------------------------- 
--- To run a lua files
+-- If then
 -------------------------------------------------- -
-:luafile %
 x = 41
 if x > 40 then
   print('over 40')
@@ -72,7 +82,9 @@ else
   print('under')
 end
 
--- verb (in init.vim)
+ -------------------------------------------------- 
+ --     Highlight on yank
+ -------------------------------------------------- 
 -- y{motion} will highlight for you!
 -- :au TextYankPost * silent! lua vim.highlight.on_yank() 
 
@@ -102,4 +114,9 @@ return M
 vim.wo.number = true
 vim.wo.number = false
 vim.bo.shiftwidth = 4    
-  
+
+
+
+-- TODO not working
+vim.spell.check("enormouz")  
+]]

85d5a4126615c8c8782b1ceabde2d90b6a6cb3fe
jimrothstein <jimrothstein@gmail.com>
2023-09-03 11:00:40 -0700

    wip

--- 670_function_as_objects.lua
+++ 670_function_as_objects.lua
@@ -3,17 +3,25 @@ local f = function()
 end
 
 print(f)
-local g = f -- g, f point to same memory;   note g = f() is differnt
 
+local g = f -- g, f point to same memory;   note g = f() is differnt; giving f() a name ('g')
 print(g)
-
+--------------------------------------------------------------------
+--  START HERE
+--  https://en.wikibooks.org/wiki/Lua_Functional_Programming/Functions
+--  run at cli:     >lua
 --
+--  anonymous
+-- return (function(x) return x*2 end)(1)
+
 -- Passing function
 local hello = function()
 	print("hello")
 end
 
+-- takes a function x, runs x() and returns value
 local h = function(x)
+	-- return "function h says " .. x() (FAILS)
 	return x()
 end
 
